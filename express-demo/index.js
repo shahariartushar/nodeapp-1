171130108b4211649ab81d953011ccf8c9968f85
shahariartushar <shahariartushar@gmail.com>
2022-01-02 02:49:36 +0100

    into to rest api

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,12 +1,56 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+//courses
+const courses =[
+    { id: 1, name: 'KTR'},
+    { id: 2, name: 'DSAM'},
+    { id: 3, name: 'PSI'}
+];
+
+
 app.get('/', (req, res)=>{
     res.send('Hello World!!');
 });
 
+//get /api/courses
 app.get('/api/courses', (req, res)=>{
-    res.send(['KTR', 'DSAM', 'PSI']);
+    res.send(courses);
 });
 
-app.listen(3000, () => console.log('Listening on port 3000...'));
+//get /api/courses/1
+app.get('/api/courses/:id', (req, res)=>{
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+
+    if (!course) res.status(404).send('Course Id not found');
+    res.send(course);
+});
+
+//post /api/courses
+app.post('/api/courses', (req, res)=>{
+    const course =  {
+        id: courses.length + 1,
+        name: req.body.name
+
+    };
+    courses.push(course);
+    res.send(course);
+});
+
+
+//multiple params /api/year/month
+app.get('/api/:year/:month', (req, res)=>{
+    res.send(req.params);
+});
+
+// /api/year/month?sortBy=name
+app.get('/api/:year/:month', (req, res)=>{
+    res.send(req.query);
+});
+
+
+//port
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
--- nodeapp-1/test.js
+++ nodeapp-1/test.js
@@ -1,1 +1,24 @@
-console.log("test");
+let exampleNode = {
+    value: 0,
+    next: {
+      value: 1,
+      next: {
+        value: 2,
+        next: null
+      }
+    }
+  };
+  
+const arr = [];
+  
+function printList(node) {
+  // Print all nodes in the linkedlist given the first node
+  while (exampleNode !== null) {
+    arr.push(exampleNode.value);
+    exampleNode = exampleNode.next;
+  }
+}
+
+
+printList(exampleNode);
+console.log(arr);
