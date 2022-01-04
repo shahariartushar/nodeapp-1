54346e915816cef3f8a8b0dd9a8ef5b40d3c7e79
shahariartushar <shahariartushar@gmail.com>
2022-01-04 01:15:45 +0100

    configuration

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,9 +1,24 @@
 const Joi = require('joi');
-
+const logger = require('./logger');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const config = require('config');
 const express = require('express');
+
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(helmet());
+
+if (app.get('env') === 'development') {
+    app.use(morgan('tiny'));
+    console.log('Morgan Enabled...');
+}
+
+
+app.use(logger);
 
 //port
 const port = process.env.PORT || 3000;
@@ -103,7 +118,7 @@ function validateCourse(course){
 
 //multiple params /api/year/month
 app.get('/api/:year/:month', (req, res)=>{
-    res.send(req.params);
+    res.send(req.params);//test message
 });
 
 // /api/year/month?sortBy=name
